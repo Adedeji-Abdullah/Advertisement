d13c5a971a0809a48facd4139193d5d4c31708f7
Chidera Chikwendu <wisdomchikwendu2005@gmail.com>
2026-08-19 18:09:13 +0100

    feat: enhance SearchCategories component layout and styling for better user experience

--- src/pages/home/SearchCategories.jsx
+++ src/pages/home/SearchCategories.jsx
@@ -2,23 +2,30 @@ export default function SearchCategories() {
   return (
     <form
       action="#"
-      className="mt-10 bg-on-primary flex flex-col gap-y-10 items-center w-full max-w-7/10 mx-auto"
+      className="mt-10 bg-on-primary rounded-4xl flex flex-col gap-y-10 items-start w-full max-w-9/10 mx-auto py-3 px-2"
     >
-      <section className="flex gap-x-2">
-        <span className="material-symbols-outlined">search</span>
-        <input type="text" placeholder="What are you looking for?" />
+      <section className="pl-7 flex flex-col gap-y-10">
+        <section className="flex gap-x-2">
+          <span className="material-symbols-outlined">search</span>
+          <input type="text" className="outline-0" placeholder="What are you looking for?" />
+        </section>
+        <section className="flex gap-x-2">
+          <span className="material-symbols-outlined">category</span>
+          <select name="categories" id="" className="cursor-pointer">
+            <option value="all-categories">All categories</option>
+            <option value="marketing">Marketing</option>
+            <option value="tech-services">Tech Services</option>
+            <option value="real-estate">Real Estate</option>
+            <option value="creative-design">Creative Design</option>
+          </select>
+        </section>
       </section>
-      <section className="flex gap-x-2">
-        <span className="material-symbols-outlined">category</span>
-        <select name="categories" id="">
-          <option value="all-categories">All categories</option>
-          <option value="marketing">Marketing</option>
-          <option value="tech-services">Tech Services</option>
-          <option value="real-estate">Real Estate</option>
-          <option value="creative-design">Creative Design</option>
-        </select>
-      </section>
-      <button type="submit">Search</button>
+      <button
+        type="submit"
+        className="bg-primary text-surface w-full rounded-full p-2 cursor-pointer hover:bg-primary-container transition-colors"
+      >
+        Search
+      </button>
     </form>
   );
 }
