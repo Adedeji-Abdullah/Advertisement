04bb1e59a854e46a36d1f910f017ebd555c8a229
Chidera Chikwendu <wisdomchikwendu2005@gmail.com>
2026-08-28 23:29:32 +0100

    feat: update PopularCategories component layout; add CategoriesGrid component and configure Prettier

--- src/pages/home/PopularCategories.jsx
+++ src/pages/home/PopularCategories.jsx
@@ -1,7 +1,21 @@
 export default function PopularCategories() {
   return (
-    <section>
-      <h1>Popular Categories</h1>
+    <section className="bg-surface-lowest mt-10 flex items-end justify-between px-7 py-8">
+      <section>
+        <h3>Popular Categories</h3>
+        <p className="font-geist text-sm text-gray-500">
+          Explore curated listings by industry
+        </p>
+      </section>
+      <a
+        href="/browse"
+        className="text-primary flex items-center gap-1 text-sm font-semibold transition-all hover:gap-2"
+      >
+        View All
+        <span className="material-symbols-outlined text-primary">
+          arrow_forward
+        </span>
+      </a>
     </section>
   );
 }
