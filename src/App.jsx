1058f40d81c82a12eca5386e4736b8ed59d60ad9
Chidera Chikwendu <wisdomchikwendu2005@gmail.com>
2026-09-01 22:13:59 +0100

    feat: update PopularCategories and CategoriesGrid components; refactor App and index styles for improved layout and consistency

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,20 @@ import Header from "./components/Header";
 import "./App.css";
 
 function App() {
-  const [backendData, setBackendData] = useState("Loading...");
+  // const [backendData, setBackendData] = useState("Loading...");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("/api/data");
-        setBackendData(response.data.message);
-      } catch (error) {
-        console.error("Error: failed to fetch data", error);
-        setBackendData("Failed to fetch backend");
-      }
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get("/api/data");
+  //       setBackendData(response.data.message);
+  //     } catch (error) {
+  //       console.error("Error: failed to fetch data", error);
+  //       setBackendData("Failed to fetch backend");
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <BrowserRouter>
--- src/pages/home/CategoriesGrid.jsx
+++ src/pages/home/CategoriesGrid.jsx
@@ -26,9 +26,9 @@ export default function CategoriesGrid() {
     },
     {
       id: 5,
-      icon: "rocket_launch",
-      title: "Startups",
-      color: "on-primary-container",
+      icon: "real_estate_agent",
+      title: "Property",
+      color: "secondary-container",
     },
     {
       id: 6,
@@ -37,4 +37,12 @@ export default function CategoriesGrid() {
       color: "on-primary-container",
     },
   ];
+  return (
+    <section className="my-3.5 px-2.5">
+      <section className="bg-on-primary flex size-32 w-full max-w-2/5 flex-col justify-center text-center">
+        <span className="material-symbols-outlined">rocket_launch</span>
+        <h3>Startups</h3>
+      </section>
+    </section>
+  );
 }
