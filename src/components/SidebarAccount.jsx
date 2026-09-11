c35ce8bb174ebc245235882b5c188d8f3c87559c
Chidera Chikwendu <wisdomchikwendu2005@gmail.com>
2026-09-11 14:33:46 +0100

    feat: update SidebarAccount component with improved layout and styling for buttons

--- src/components/SidebarAccount.jsx
+++ src/components/SidebarAccount.jsx
@@ -1,9 +1,10 @@
+import { NavLink } from "react-router-dom";
 import ProfileDP from "../assets/profile-dp.jpg";
 
 export default function SidebarAccount() {
   return (
-    <section className="absolute bottom-0 flex flex-col items-center justify-center gap-4 border-t border-t-gray-300 pt-10">
-      <section className="flex">
+    <section className="absolute bottom-0 flex flex-col items-center justify-center gap-4 border-t border-t-gray-300 py-10">
+      <section className="bg-inverse-on-surface flex gap-3 rounded-2xl px-5 py-3 font-semibold">
         <img
           src={ProfileDP}
           alt="profile picture"
@@ -11,13 +12,23 @@ export default function SidebarAccount() {
         />
         <div>
           <p>Alex Morgan</p>
-          <a href="mailto:alex@advantamarket.com">alex@advantamarket.com</a>
+          <a href="mailto:alex@advantamarket.com" className="text-gray-600">
+            alex@advantamarket.com
+          </a>
         </div>
       </section>
-      <button>Post an Ad</button>
-      <button className="flex items-center">
-        <span class="material-symbols-outlined">logout</span>Log Out
-      </button>
+      <NavLink
+        to="/"
+        className="bg-primary text-surface hover:bg-primary-container mx-auto w-full rounded-2xl px-2 py-3 text-center text-base font-bold transition-colors"
+      >
+        Post an Ad
+      </NavLink>
+      <NavLink
+        to="/"
+        className="flex w-full items-center justify-center rounded-2xl border border-gray-400 px-2 py-3 transition-colors hover:bg-gray-100"
+      >
+        <span className="material-symbols-outlined">logout</span>Log Out
+      </NavLink>
     </section>
   );
 }
