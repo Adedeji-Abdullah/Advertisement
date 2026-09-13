501e8fbccc459992e619c9abaa0b3fb95104da72
Chidera Chikwendu <wisdomchikwendu2005@gmail.com>
2026-09-13 21:13:11 +0100

    feat: enhance FeaturedAdsGrid layout with improved styling and transitions

--- src/pages/home/FeaturedAdsGrid.jsx
+++ src/pages/home/FeaturedAdsGrid.jsx
@@ -3,9 +3,15 @@ import SaasAnalytics from "../../assets/images/saas_analytics.jpg";
 
 export default function FeaturedAdsGrid() {
   return (
-    <section className="grid">
-      <section className="">
-        <img src={SaasAnalytics} alt="saas analytics image" />
+    <section className="group mx-4 my-6 grid overflow-hidden rounded-2xl bg-white">
+      <img
+        src={SaasAnalytics}
+        alt="saas analytics image"
+        className="w-full transition-transform duration-300 group-hover:scale-110"
+      />
+      <section className="flex justify-between p-4">
+        <h3>Saas Analytics...</h3>
+        <h3>$12,500</h3>
       </section>
     </section>
   );
